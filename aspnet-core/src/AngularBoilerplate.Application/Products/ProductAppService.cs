5751789236e27e90f07aef4af30547e2c8f4d967
aleem <aleem604@gmail.com>
2022-01-14 00:45:23 +0500

    small refactoring

--- aspnet-core/src/AngularBoilerplate.Application/Products/ProductAppService.cs
+++ aspnet-core/src/AngularBoilerplate.Application/Products/ProductAppService.cs
@@ -1,11 +1,5 @@
 ﻿using AngularBoilerplate.Permissions;
 using Microsoft.AspNetCore.Authorization;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
